4d7670c2b37b5e58949cfb6648192b2adc8a7d62
JK4302 <72574346+JK432@users.noreply.github.com>
2022-01-07 10:55:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Write a Python Program to add, sunstract and multiply complex numbers using functions. 
+
+def cmplex(r1,c1,r2,c2):
+
+  print((r1+r2),"+",(c1+c2),"i")
+  print((r1-r2),"+",(c1-c2),"i")
+  print((r1*r2),"+",(c1*c2),"i")
+  print((r1/r2),"+",(c1/c2),"i")
+
+real=int(input())
+comlx= int(input())
+
+
+
+
+real1=int(input())
+comlx1= int(input())
+
+
+
+cmplex(real,comlx,real1,comlx1)
